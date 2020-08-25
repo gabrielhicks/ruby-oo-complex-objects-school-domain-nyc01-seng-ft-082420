8621a94b65335832e91914f731e5024bfe2793e4
gabrielhicks <gabrielhicks@mail.com>
2020-08-25 17:57:45 -0400

    created a domain model for a school

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade] == nil
+            @roster[grade] = []
+        end
+        @roster[grade] << name
+    end
+
+    def grade(year)
+        @roster[year]
+    end
+    
+    def sort
+        sorted_students = {}
+        @roster.each do |grade, names|
+            sorted_students[grade] = names.sort
+        end
+        sorted_students
+    end
+
+end
